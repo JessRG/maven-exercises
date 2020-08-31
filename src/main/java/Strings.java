ce3ad1872daf33c46f0394f658e557f93493061b
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-08-31 15:22:21 -0500

    Added the Apache Commons Lang library to the project and imported the StringUtils class for string manipulation. Completed this Java II: Dependency Management exercise.

--- src/main/java/Strings.java
+++ src/main/java/Strings.java
@@ -1,7 +1,22 @@
-import org.apache.commons.codec.binary.StringUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
 
 public class Strings {
     public static void main(String[] args) {
-        StringUtils
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter something: ");
+        String input = scan.nextLine();
+
+        System.out.printf("You Entered: \"%s\"\n", input);
+
+        //Tells whether or not what the user entered is a number
+        System.out.printf("\"%s\" %s a number\n", input, StringUtils.isAlpha(input) ? "is" : "is not");
+
+        //Flips the case of the string
+        System.out.printf("Flipped Case: %s\n", StringUtils.swapCase(input));
+
+        //Reverses the string
+        System.out.printf("Reversed: %s\n", StringUtils.reverse(input));
     }
 }
